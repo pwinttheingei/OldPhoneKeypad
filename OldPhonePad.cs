f752fad73bd12fa474ac785636b6174fd9c25c30
Yin Yin Mon <41563676+YinYinMon@users.noreply.github.com>
2024-10-23 16:05:57 +0700

    Add files via upload
    Old Phone Keypad _ C# Code Challenge

---
+++ OldPhonePad.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OldPhonePadConverter
+{
+    public class OldPhonePad
+    {
+        private static readonly Dictionary<char, string> PhoneKeyMapping = new Dictionary<char, string>
+        {
+            { '0'," "},
+            { '1', ""},
+            { '2', "ABC" },
+            { '3', "DEF" },
+            { '4', "GHI" },
+            { '5', "JKL" },
+            { '6', "MNO" },
+            { '7', "PQRS" },
+            { '8', "TUV" },
+            { '9', "WXYZ" }
+        };
+
+        public string ConvertInput(string input)
+        {
+            if (string.IsNullOrEmpty(input) || !input.EndsWith("#"))
+            {
+                throw new ArgumentException("Input must end with #.");
+            }
+            //// If the input doesn't end with #, append it
+            //if (!input.EndsWith("#"))
+            //{
+            //    input += "#";
+            //}
+
+            StringBuilder output = new StringBuilder();
+            char[] inputChars = input.TrimEnd('#').ToCharArray();
+            int length = inputChars.Length;
+
+            for (int i = 0; i < length; i++)
+            {
+                char currentChar = inputChars[i];
+                int count = 1;
+
+                // Count repeated characters
+                while (i + 1 < length && inputChars[i + 1] == currentChar)
+                {
+                    count++;
+                    i++;
+                }
+
+                // Determine which letter to append to the output
+                if (PhoneKeyMapping.ContainsKey(currentChar))
+                {
+                    string letters = PhoneKeyMapping[currentChar];
+                    // Use modulo to wrap around if count exceeds the number of letters
+                    char letter = letters[(count - 1) % letters.Length];
+                    output.Append(letter);
+                }
+            }
+
+            return output.ToString();
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using OldPhonePadConverter;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        OldPhonePad phonePad = new OldPhonePad();
+
+        while (true) // Repeat until valid input is provided
+        {
+            Console.Write("Enter input for the Old Phone Pad (end with #): ");
+            string userInput = Console.ReadLine();
+
+            // Check if the input ends with '#'
+            if (!userInput.EndsWith("#"))
+            {
+                Console.WriteLine("Please make sure to add '#' at the end of your input.");
+            }
+            else
+            {
+                try
+                {
+                    // Process the input and display the output
+                    string result = phonePad.ConvertInput(userInput);
+                    Console.WriteLine("Output: " + result);
+                    break; // Exit the loop when input is valid
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error: " + ex.Message);
+                }
+            }
+        }
+    }
+}
